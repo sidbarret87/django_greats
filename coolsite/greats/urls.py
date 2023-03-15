3d0005599e6a66e755a2560387e8e03e9ab6f0f4
sidbarret87 <sidbarret@mail.ru>
2023-03-15 21:47:06 +0300

    6. Шаблоны (templates).

--- coolsite/greats/urls.py
+++ coolsite/greats/urls.py
@@ -3,7 +3,8 @@ from greats.views import *
 
 urlpatterns = [
     path('', index, name='home'),
-    path('cats/<int:catid>/',categories),
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive)
+    # path('cats/<int:catid>/',categories),
+    # re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
+    path('about/', about, name='about')
 ]
 
--- coolsite/greats/views.py
+++ coolsite/greats/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
-
+from .models import *
+menu =['О сайте','Добавить статью', 'Обратная связь', "Войти"]
 def index(request):
-    return HttpResponse("Страница приложения greats.")
-
+    posts= Greats.objects.all().order_by('title')
+    return render (request, 'greats/index.html', {'posts':posts,'menu':menu,'title':'Главная страница.'})
+def about(request):
+    return render(request, 'greats/about.html', {'menu':menu,'title':'О сайте.'})
 def categories(request, catid):
     if request.GET:
         print(request.GET)
