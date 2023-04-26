37bd874c0b8416441aae480b3b8b5dfcd5dc3e24
sidbarret87 <sidbarret@mail.ru>
2023-04-26 19:56:44 +0300

    14 -  Формы, связанные с моделями пользовательские валидаторы завершено

--- coolsite/greats/views.py
+++ coolsite/greats/views.py
@@ -25,14 +25,12 @@ def about(request):
 # Create your views here.
 def addpage(request):
      if request.method=='POST':
-         form = AddPostForm(request.POST)
+         form = AddPostForm(request.POST, request.FILES)
          if form.is_valid():
              #print(form.cleaned_data)
-             try:
-                 Greats.objects.create(**form.cleaned_data)
+
+                 form.save()
                  return redirect('home')
-             except:
-                 form.add_error(None,"Ошибка добавления поста")
 
      else:
          form = AddPostForm()
